ce08f78f9e69be00d30a389f5f5e61c228f814f6
Diego (D) <actual.mensch0r@icloud.com>
2022-09-23 11:29:11 -0500

    Add 1st game + routes
    Co-authored-by: Josh Durrett <boonaki@users.noreply.github.com>

---
+++ pages/games/GameContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GameContainer = () => {
+  return (
+    <div className='px-48 py-12 flex justify-center items-center'>
+      <iframe frameborder="0" width="600px" height="750px" src="https://GroupTicTacToe-with-Login.dailydisco.repl.co?embed=true"></iframe>
+      {/* <iframe frameborder="0" width="100%" height="700px" src="https://squaretag.netlify.app/"></iframe> */}
+    </div>
+  )
+}
+
+export default GameContainer
